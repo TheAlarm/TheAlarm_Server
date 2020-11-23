d909e074e12dc923d7fadde98e6e377d8db0c6dc
ChoSooMin <soom9611@gmail.com>
2020-11-24 00:21:12 +0900

    feat: Get all promise

--- src/controllers/promiseController.js
+++ src/controllers/promiseController.js
@@ -1,5 +1,7 @@
 // const { query } = require('../../../config/database');
 // const { logger } = require('../../../config/winston');
+const { getMysqlPool } = require('../../modules/database');
+const database = require('../../modules/database');
 const utils = require('../../modules/response')
 
 /**
@@ -8,5 +10,37 @@ const utils = require('../../modules/response')
  */
 exports.getPromiseAll = async function (req, res) {
     console.log("다짐목록 조회 API")
-    return res.send(utils.successFalse(400, "유저아이디가 존재하지않습니다."));
+
+    try {
+        const getPromiseAllQuery = `SELECT * FROM promise`
+        const result = await database.startTransaction(getPromiseAllQuery)
+
+        if (result.status == "success") {
+            return res.send(utils.successTrue(200, result.status, result.connection));
+        }
+        return res.send(utils.successTrue(200, "조회 성공", ))
+    } catch (err) {
+        console.log(err)
+    }
+    // return res.send(utils.successFalse(400, "유저아이디가 존재하지않습니다."));
+}
+
+exports.postPromise = async function (req, res) {
+    console.log("다짐 쓰기 API")
+
+    try {
+        const {
+            promise
+        } = req.body
+
+        if (!promise) {
+            return res.send(utils.successFalse(400, "다짐을 작성해주세요"))
+        }
+
+        // const insertPromiseQuery = `INSERT INTO promise (promise, createdAt, public, userIdx) VALUES ?`
+        // database.query(insertPromiseQuery, [])
+    } catch (err) {
+        console.log(err)
+    }
+
 }
--- src/routes/promiseRoute.js
+++ src/routes/promiseRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const promise = require('../controllers/promiseController');
 
 router.get("/app/promise-list", promise.getPromiseAll);
+router.post("/app/promise", promise.postPromise);
 
 module.exports = router;
