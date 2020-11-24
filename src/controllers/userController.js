5a42f6e5ef3c34eeab8fecc0f60d6028f3cd894f
You-hyeonyeong <dbgusdud412@naver.com>
2020-11-24 18:53:46 +0900

    디비 연동 테스트

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,16 @@
-// const { query } = require('../../../config/database');
-// const { logger } = require('../../../config/winston');
+const { query } = require('../../modules/dbModule');
 const utils = require('../../modules/response')
+
 
 /**
  * 2020.11.23
  * 회원가입 API
  */
 exports.signUp = async function (req, res) {
+    const result = await query(`INSERT INTO userInfo (name, email, type) VALUES ('혀녀이','hy@gmail.com', 'kakao')`);
     console.log("회원가입 API")
-    return res.send(utils.successTrue(200, "회원가입 성공"));
+
+    return res.send(utils.successTrue(200, "회원가입 성공", result));
 }
 
 exports.signIn = async function (req, res) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use("/", require("./promiseRoute"));
-router.use("/", require("./userRoute"));
+router.use("/user", require("./userRoute"));
 
 module.exports = router;
