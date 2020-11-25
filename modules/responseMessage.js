aa14235fc9df2fea3fb49ff2906f16120697c0c8
ChoSooMin <soom9611@gmail.com>
2020-11-25 17:58:14 +0900

    feat: 다짐목록 조회 API

---
+++ modules/responseMessage.js
@@ -0,0 +1,11 @@
+// responseMessage.js
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+    BODY_VALUE_ERROR: "Body 타입이 잘못 되었습니다.",
+    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+    DUPLICATE_VALUE_ERROR: "파라미터 값 중복",
+    SIGN_UP_SUCCESS: "회원가입 성공",
+    SIGN_IN_SUCCESS: "로그인 성공",
+    POSSIBLE_NICKNAME: "사용 가능한 닉네임입니다."
+}
--- src/controllers/promiseController.js
+++ src/controllers/promiseController.js
@@ -1,46 +1,52 @@
-// const { query } = require('../../../config/database');
 // const { logger } = require('../../../config/winston');
-const { getMysqlPool } = require('../../modules/dbModule');
-const database = require('../../modules/dbModule');
-const utils = require('../../modules/response')
+const { query } = require('../../modules/dbModule');
+const utils = require('../../modules/response');
+const statusCode = require('../../modules/statusCode');
+const responseMessage = require('../../modules/responseMessage');
 
 /**
  * 2020.11.21
  * 다짐목록 조회
  */
-exports.getPromiseAll = async function (req, res) {
-    console.log("다짐목록 조회 API")
+exports.getAllPromise = async function (req, res) {
+    console.log("다짐목록 조회 API");
 
     try {
-        const getPromiseAllQuery = `SELECT * FROM promise`
-        const result = await database.startTransaction(getPromiseAllQuery)
+        const getAllPromiseQuery = `SELECT * FROM promise`;
+        const result = await query(getAllPromiseQuery);
 
-        if (result.status == "success") {
-            return res.send(utils.successTrue(200, result.status, result.connection));
+        const promiseList = new Array();
+
+        for (var i = 0; i < result.length; i++) {
+            promiseList.push(result[i].promise);
         }
-        return res.send(utils.successTrue(200, "조회 성공", ))
+
+        const json = utils.successTrue(200, "다짐목록 조회", promiseList);
+
+        return res.status(statusCode.OK).send(json);
     } catch (err) {
-        console.log(err)
+        console.log(err);
+        
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     }
     // return res.send(utils.successFalse(400, "유저아이디가 존재하지않습니다."));
 }
 
 exports.postPromise = async function (req, res) {
-    console.log("다짐 쓰기 API")
+    console.log("다짐 쓰기 API");
 
     try {
         const {
             promise
-        } = req.body
+        } = req.body;
+
+
 
         if (!promise) {
-            return res.send(utils.successFalse(400, "다짐을 작성해주세요"))
+            return res.send(utils.successFalse(400, "다짐을 작성해주세요"));
         }
-
-        // const insertPromiseQuery = `INSERT INTO promise (promise, createdAt, public, userIdx) VALUES ?`
-        // database.query(insertPromiseQuery, [])
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
 
 }
