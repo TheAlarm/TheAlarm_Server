f37c23faeaac020532a8b9399b9c6a4b971fbdfa
ChoSooMin <soom9611@gmail.com>
2020-12-22 21:12:11 +0900

    feat: 날짜별 다짐 목록 조회 가능

--- src/controllers/promiseController.js
+++ src/controllers/promiseController.js
@@ -6,8 +6,8 @@ const responseMessage = require('../../modules/responseMessage');
 const moment = require('moment');
 
 /**
- * 2020.11.21
- * 다짐목록 조회
+ * [GET] /app/promise-list
+ * @author ChoSooMin
  */
 exports.getAllPromise = async function (req, res) {
     console.log("다짐목록 조회 API");
@@ -24,7 +24,32 @@ exports.getAllPromise = async function (req, res) {
         
         return res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     }
-}
+};
+
+exports.getDatePromiseList = async function (req, res) {
+    console.log("날짜별 다짐 목록 조회 API");
+
+    try {
+        const {
+            date
+        } = req.params;
+
+        if (!date) {
+            res.status(statusCode.BAD_REQUEST).send(utils.successFalse(statusCode.BAD_REQUEST, "날짜를 입력해주세요"));
+        }
+
+        const getDatePromiseListQuery = `SELECT * FROM promise WHERE DATE(createdAt) = ?`;
+
+        const result = await query(getDatePromiseListQuery, [date]);
+        const json = utils.successTrue(200, "날짜별 다짐 목록 조회", result);
+
+        return res.status(statusCode.OK).send(json);
+    } catch (err) {
+        console.log(err);
+
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    }
+};
 
 /**
  * 
@@ -61,5 +86,4 @@ exports.postPromise = async function (req, res) {
 
         return res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     }
-
-}
+};
--- src/routes/promiseRoute.js
+++ src/routes/promiseRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const promise = require('../controllers/promiseController');
 const jwtMiddleware = require('../../modules/jwtMiddleware')
 
+// 다짐 목록 조회는 토큰 없이 가능
 router.get("/app/promise-list", promise.getAllPromise); // 다짐목록 조회 (get)
 router.post("/app/promise", jwtMiddleware, promise.postPromise); // 다짐 작성 (post)
+router.get("/app/promise-list/:date", promise.getDatePromiseList); // 날짜별 다짐목록 조회 (get)
 
 module.exports = router;
