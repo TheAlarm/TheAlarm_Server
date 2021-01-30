747821d51e188f5d470276ffd5d3dfc728e8e9f3
You-hyeonyeong <dbgusdud412@naver.com>
2021-01-30 21:16:05 +0900

    내 정보 수정 API

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const user = require('../controllers/userController');
 const fb = require('../../modules/facebook-jwt');
 // profile 사진 변경 (multer)
-const upload = require('../../config/multer');
+//const upload = require('../../config/multer');
 const jwtMiddleware = require('../../modules/jwtMiddleware');
 
 router.post("/sign-up", user.signUp);
@@ -14,7 +14,8 @@ router.post("/kakao", user.kakaoLogin);                     // 토큰 받아서 
 router.get("/user/kakao-redirect", user.kakaoRedirect);     //리다리렉션 코드
 
 router.post('/facebook', fb, user.facebook);
+router.patch("/user", jwtMiddleware, user.profileEdit);
 
-router.put('/userInfo/editProfile', jwtMiddleware, upload.single('profile'), user.editProfile);
+//router.put('/userInfo/editProfile', jwtMiddleware, upload.single('profile'), user.editProfile);
 
 module.exports = router;
